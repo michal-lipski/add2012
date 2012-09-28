d4378cd8378a00e1dd0a7619e4e57eb6d48608b9
Michal Lipski <kiwaczki@gmail.com>
2012-09-28 22:07:51 +0200

    refactoring skills panel model to object

--- src/main/java/add/haslearntit/application/skills/NewSkillPanel.java
+++ src/main/java/add/haslearntit/application/skills/NewSkillPanel.java
@@ -3,56 +3,52 @@ package add.haslearntit.application.skills;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.Model;
 
 import add.haslearntit.domain.skills.Skill;
 import add.haslearntit.domain.skills.SkillsRepository;
 
-public class NewSkillPanel extends Panel{
+public class NewSkillPanel extends Panel {
 
 	private static final long serialVersionUID = 2627832833454321010L;
-
-	private Model<String> nameModel = Model.of();
-	private Model<String> difficultyModel = Model.of();
-	private Model<String> timeModel = Model.of();
 
 	private final SkillsRepository skillsRepository;
 
 	public NewSkillPanel(String id, SkillsRepository skillsRepository) {
-		
+
 		super(id);
 		this.skillsRepository = skillsRepository;
-		add(new NewSkillFrom("newSkillForm"));
+		SkillModel skillModel = new SkillModel();
+		add(new NewSkillFrom("newSkillForm", new CompoundPropertyModel<SkillModel>(skillModel)));
 	}
 
 	private class NewSkillFrom extends Form {
 		private static final long serialVersionUID = -3821156411930577907L;
 
-		public NewSkillFrom(String id) {
-			super(id);
+		public NewSkillFrom(String id, CompoundPropertyModel<SkillModel> compoundPropertyModel) {
+			super(id, compoundPropertyModel);
 			initializeComponents();
 		}
 
 		private void initializeComponents() {
-			add(new RequiredTextField<String>("name", nameModel));
-			add(new RequiredTextField<String>("difficulty", difficultyModel));
-			add(new RequiredTextField<String>("time", timeModel));
+			add(new RequiredTextField<String>("name"));
+			add(new RequiredTextField<String>("difficulty"));
+			add(new RequiredTextField<String>("time"));
 		}
 
 		@Override
 		protected void onSubmit() {
-
-			skillsRepository.store(
-					new Skill(
-							nameModel.getObject(),
-							difficultyModel.getObject(),
-							timeModel.getObject())
-			);
-
-			nameModel.setObject("");
-			difficultyModel.setObject("");
-			timeModel.setObject("");
+			SkillModel model = (SkillModel) this.getDefaultModelObject();
+			skillsRepository.store(new Skill(model.name, model.difficulty, model.time));
+			this.setDefaultModelObject(new SkillModel());
 		}
+	}
+
+	private class SkillModel {
+		private String name;
+		private String difficulty;
+		private String time;
 	}
 
 }
