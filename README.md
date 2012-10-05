933fdec706b5be161edb24b01a9aa6327ddff2db
Michal Lipski <kiwaczki@gmail.com>
2012-10-05 20:42:37 +0200

    merge with upstream

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 [![Build Status](https://buildhive.cloudbees.com/job/agiledevday/job/add2012/badge/icon)](https://buildhive.cloudbees.com/job/agiledevday/job/add2012/)
-d
-add201d2
-=======cc
-ddfff
+
+add2012
+=======
+
+http://add2012.herokuapp.com/
--- src/test/java/add/haslearntit/application/skills/LearntSkillsListTest.java
+++ src/test/java/add/haslearntit/application/skills/LearntSkillsListTest.java
@@ -4,22 +4,13 @@ import static java.util.Arrays.asList;
 
 import java.util.List;
 
+import add.haslearntit.HasLearntItBaseWicketIT;
 import org.apache.wicket.model.util.ListModel;
-import org.apache.wicket.util.tester.WicketTester;
-import org.junit.Before;
 import org.junit.Test;
 
-import add.haslearntit.application.HasLearntItApplication;
 import add.haslearntit.domain.skills.Skill;
 
-public class LearntSkillsListTest {
-
-	private WicketTester tester;
-
-	@Before
-	public void setUp() {
-		tester = new WicketTester(new HasLearntItApplication());
-	}
+public class LearntSkillsListTest extends HasLearntItBaseWicketIT {
 
 	@Test
 	public void shouldDisplayAllLearntSkills() throws Exception {
@@ -58,6 +49,18 @@ public class LearntSkillsListTest {
 
 		// then:
 		tester.assertContainsNot("You haven't recorded any skills. For sure there is something you have learnt lately!");
+	}
+	
+	@Test
+	public void shouldDisplayLearnPoints(){
+		//given
+		Skill someSkill = new Skill("java programming", "EASY", "1");
+		
+		//when
+		tester.startComponentInPage(new LearntSkillsList("learntSkillsList", modelContainingSkills(someSkill)));
+		
+		//then
+		tester.assertLabel("learntSkillsList:list:0:skillPoints",Integer.toString(someSkill.getEarnedPoints()));
 	}
 	
 	// --
